02f59f6def94c44d8a2e8dc9f2097cca358bb400
Louis <jovanhan2@gmail.com>
2019-02-14 13:13:38 +0000

    send simple message to local server

--- app/src/main/java/com/example/caterpillar/LoginActivity.java
+++ app/src/main/java/com/example/caterpillar/LoginActivity.java
@@ -2,15 +2,63 @@ package com.example.caterpillar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.engineio.client.Transport;
+import com.github.nkzawa.socketio.client.Manager;
+import com.github.nkzawa.socketio.client.Socket;
+import com.server.interaction.SocketManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+    private String mUsername;
 
+    private Socket mSocket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SocketManager app = (SocketManager) getApplication();
+        mSocket = app.getmSocket();
+        mSocket.io().on(Manager.EVENT_TRANSPORT, new Emitter.Listener() {
+            @Override
+            public void call(Object... args) {
+                Transport transport = (Transport) args[0];
+                transport.on(Transport.EVENT_ERROR, new Emitter.Listener() {
+                    @Override
+                    public void call(Object... args) {
+                        Exception e = (Exception) args[0];
+                        Log.e("socketConnection", "Transport error " + e);
+                        e.printStackTrace();
+                        e.getCause().printStackTrace();
+                    }
+                });
+            }
+        });
+        // Set up the login form.
+        mUsernameView = findViewById(R.id.input_user);
+        mPasswordView= findViewById(R.id.input_password);
+
+//        Button signInButton = (Button) findViewById(R.id.loginButton);
+//        signInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                attemptLogin();
+//            }
+//        });
+
+
+
     }
 
     public void onClickCreateAccount(View view){
@@ -19,6 +67,11 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void onClickLogin(View view){
+//        Map<String, String> loginDetails = new HashMap<>();
+//        loginDetails.put("Login", mUsernameView.getText().toString());
+//        loginDetails.put("Password", mPasswordView.getText().toString());
+        mSocket.emit("new message", mUsernameView.getText().toString());
+        Log.i("socket", "sent login details");
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
---
+++ app/src/main/java/com/server/interaction/SocketManager.java
@@ -0,0 +1,28 @@
+package com.server.interaction;
+import android.app.Application;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+import java.net.URISyntaxException;
+
+//  keep the global state of my application
+public class SocketManager extends Application {
+    private Socket mSocket;
+    private static final String URL = "http://146.169.168.81:65080/";
+    public SocketManager(){
+        //initialize maybe with default values if any based on use case
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            mSocket = IO.socket(URL);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        mSocket.connect();
+    }
+    public Socket getmSocket(){
+        return mSocket;
+    }
+}
