9f86a694a579bcdd91d0a21cd91a68f995e46f20
elisaherme <elisaherme@gmail.com>
2019-02-16 20:44:33 +0000

    signup to questions activity link working

--- app/src/main/java/com/example/caterpillar/SignUpActivity.java
+++ app/src/main/java/com/example/caterpillar/SignUpActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -11,10 +12,21 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button registerButton = findViewById(R.id.register_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, QuestionsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void onClickRegister(View view){
-        Intent intent = new Intent(this, QuestionsActivity.class);
-        startActivity(intent);
-    }
+//    public void onClickRegister(View view){
+//        Intent intent = new Intent(this, QuestionsActivity.class);
+//        startActivity(intent);
+//    }
+
+
+
 }
