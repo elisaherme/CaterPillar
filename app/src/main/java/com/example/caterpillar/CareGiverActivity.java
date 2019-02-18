2ed4435ce8e89cffa391e98e64ad9dc300572ad2
Kavin Winson <kunkavin@gmail.com>
2019-02-18 02:18:22 +0000

    Add CareGiver activity

---
+++ app/src/main/java/com/example/caterpillar/CareGiverActivity.java
@@ -0,0 +1,18 @@
+package com.example.caterpillar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class CareGiverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_care_giver);
+    }
+
+}
