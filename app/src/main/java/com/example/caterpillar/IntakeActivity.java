6c22feaef8ed2a2c959141bcf489ac9580381783
Kavin Winson <kunkavin@gmail.com>
2019-03-12 14:58:22 +0000

    Add warning listeners for correct/wrong lid opened

--- app/src/main/java/com/example/caterpillar/IntakeActivity.java
+++ app/src/main/java/com/example/caterpillar/IntakeActivity.java
@@ -22,18 +22,81 @@ public class IntakeActivity extends AppCompatActivity {
     private final String [] days = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
     private final String [] times = {"Mor", "Aft", "Ngt"};
     private final String [] fulltimes = {"Morning", "Afternoon", "Night"};
-    private Button b1;
+    private TextView textMedication;
+    private ImageView imageWarning;
+    private TextView textWarning;
+    private Button buttonDismiss;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intake);
+
+        textMedication = findViewById(R.id.textPill);
+
+        imageWarning = findViewById(R.id.imageWarning);
+        textWarning = findViewById(R.id.textWarning);
+        buttonDismiss = findViewById(R.id.taken_button);
+
+        SocketManager app = (SocketManager) getApplication();
+        mSocket = app.getmSocket();
+        mSocket.on("wrong_lid", wrongLid);
+        mSocket.on("correct_lid", correctLid);
     }
 
     public void onClickTaken(View view) {
         Intent intent = new Intent(this, pillbox.class);
+
+//        // send
+//        Bundle b = new Bundle();
+//        b.putInt("med", 1); //Your id
+//        intent.putExtras(b); //Put your id to your next Intent
+//        startActivity(intent);
+//
+//        // receive
+//        Bundle b = getIntent().getExtras();
+//        int value = -1; // or other values
+//        if(b != null)
+//            value = b.getInt("key");
+
+
+
+
+
+
         startActivity(intent);
     }
+
+    private Emitter.Listener correctLid = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e("CORRECT", "correct lid");
+                    imageWarning.setVisibility(View.GONE);
+                    textWarning.setVisibility(View.GONE);
+                    buttonDismiss.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener wrongLid = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e("WRONG", "wrong lid");
+                    imageWarning.setVisibility(View.VISIBLE);
+                    textWarning.setVisibility(View.VISIBLE);
+                    buttonDismiss.setVisibility(View.GONE);
+                }
+            });
+        }
+    };
 
 //    private Emitter.Listener fillPillbox = new Emitter.Listener() {
 //        @Override
